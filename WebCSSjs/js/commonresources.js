1a756e7975a705f50af3569f9a5a2f06963e06c6
Justin Harris <jharris@paloaltonetworks.com>
2018-08-14 08:58:55 +0100

    js url moved to seperate file

---
+++ WebCSSjs/js/commonresources.js
@@ -0,0 +1,6 @@
+        // Global variable declarations used by create and delete functions
+        
+        var TransitUrlBase = 'https://9txqzb44ml.execute-api.eu-west-1.amazonaws.com/Production';
+        var SubscribingUrlBase = 'https://o5iunvk6m1.execute-api.eu-west-1.amazonaws.com/Production';
+        var LambdaSnsTopic = 'arn:aws:sns:eu-west-1:304191265855:subscriber-web-api-v3-apiAsyncCreateLambdaTopic-1KO6VLAS52J1H';
+        var SubscriberAssumeRoleArn = 'arn:aws:iam::304191265855:role/SubscriberAssumeRole-subscriber-stack';
